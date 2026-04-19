a1b37e33482ffb6b46aa1d7d616cd02b0e84b839
jothisree19 <jothisreeannadurai@gmail.com>
2026-04-19 12:05:53 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/leet.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int[] nums = {2,3,1,1,4};
+        System.out.println(obj.canJump(nums));
+    }
+}
+
+
