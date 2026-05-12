fa57019ae9609873087e7714eeceefc4d0b8f727
jothisree19 <jothisreeannadurai@gmail.com>
2026-05-12 22:18:37 +0530

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,34 @@
+package demo;
+
+public class Question {
+
+
+
+
+	    int id;
+	    String question;
+	    String opt1;
+	    String opt2;
+	    String opt3;
+	    String opt4;
+	    int answer;
+
+	    public Question(int id,
+	                    String question,
+	                    String opt1,
+	                    String opt2,
+	                    String opt3,
+	                    String opt4,
+	                    int answer) {
+
+	        this.id = id;
+	        this.question = question;
+	        this.opt1 = opt1;
+	        this.opt2 = opt2;
+	        this.opt3 = opt3;
+	        this.opt4 = opt4;
+	        this.answer = answer;
+	    }
+	}
+
+
